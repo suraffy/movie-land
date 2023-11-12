ecbd9ed5c6fbdf0e2f22d9855b5a97f3081ced22
Surafel Araya <surafelay@gmail.com>
2023-11-12 05:47:38 +0300

    Counter App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Counter App has started!");
+  }, []);
+
+  useEffect(() => {
+    console.log("Count has changed!");
+  }, [count]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>-</button>
     </div>
   );
-}
+};
 
 export default App;
