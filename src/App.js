f31228c5d1402d649f9c6cf9606867d3f4b19b40
Surafel Araya <surafelay@gmail.com>
2023-11-12 07:09:51 +0300

    Fetch movies - API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=70bf54b8";
 
 const App = () => {
+  useEffect(() => {
+    const searchMovies = async (title) => {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+
+      console.log(data.Search);
+    };
+
+    searchMovies("spiderman");
+  }, []);
+
   return <h1>App</h1>;
 };
 
