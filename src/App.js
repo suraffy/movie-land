2602a8d106881fda4e057895d74bf1af6207514a
Surafel Araya <surafelay@gmail.com>
2023-11-12 08:08:20 +0300

    Extracting Movie Card into a Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import MovieCard from "./components/MovieCard";
 import "./App.css";
 import searchIcon from "./search.svg";
 
@@ -36,31 +36,12 @@ const App = () => {
           onChange={() => {}}
           placeholder="Search movies"
         />
+
         <img src={searchIcon} alt="search" onClick={() => {}} />
       </div>
 
       <div className="container">
-        <div className="movie">
-          <div>
-            <p>{movie.Year}</p>
-          </div>
-
-          <div>
-            <img
-              src={
-                movie.Poster !== "N/A"
-                  ? movie.Poster
-                  : "https://via.placeholder.com/400"
-              }
-              alt={movie.Title}
-            />
-          </div>
-
-          <div>
-            <span>{movie.Type}</span>
-            <h3>{movie.Title}</h3>
-          </div>
-        </div>
+        <MovieCard movie={movie} />
       </div>
     </div>
   );
